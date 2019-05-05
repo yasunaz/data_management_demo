c10146d60a7905e90b2b0fa1925fbd55a14d4727
Nijat Muhtar <nijatmn@gmail.com>
2019-05-05 12:45:54 -0400

    Created build message to the system

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -5,7 +5,8 @@ import com.github.javafaker.Faker;
 public class Demo {
 	public static void main(String[] args) {
 		Faker faker = new Faker();
-		
+		System.out.println("This is the testst that we will be using for the ");
+		System.out.println("Test Execution of our web applicaiton");
 		System.out.println( faker.name().firstName() );
 		System.out.println( faker.company().name()   );
 		System.out.println( faker.address().countryCode() );
